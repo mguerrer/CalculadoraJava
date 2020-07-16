c719c35debbb322a202dc7cb056363b1e484fc67
mguerrero <marcosguerrerow@hotmail.com>
2020-07-16 19:07:24 -0400

    first commit

---
+++ src/test/java/cl/calculadora/java/UnitTests.java
@@ -0,0 +1,22 @@
+package cl.calculadora.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class UnitTests {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }    
+    @Test
+    void testApp2() {
+        assertEquals(1, 1);
+    }
+}
